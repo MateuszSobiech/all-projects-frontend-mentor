37835742288563381490522b26fac74c186dcb61
Mateusz Sobiech <lordpython8@gmail.com>
2024-11-08 15:17:08 +0100

    Add desktop styles

--- static-job-listings-master/src/components/JobItem/JobItem.tsx
+++ static-job-listings-master/src/components/JobItem/JobItem.tsx
@@ -12,25 +12,29 @@ export const JobItem = ({ job, onClickAddFilter }: JobItemProps) => {
   const categories = [job.role, job.level, ...job.languages, ...job.tools];
 
   return (
-    <article className={`${styles.article} ${job.new ? styles.borderLeft : ''}`}>
-      <img src={job.logo} alt={`${job.company} logo`} />
-      <div className={styles.info}>
-        <div className={styles.badges}>
-          <p className={styles.company}>{job.company}</p>
-          {job.new && <Badge variant='primary' text='new!' />}
-          {job.featured && <Badge variant='dark' text='featured' />}
+    <article className={`${styles.article} ${job.featured ? styles.borderLeft : ''}`}>
+      <div className={styles.leftContainer}>
+        <img src={job.logo} alt={`${job.company} logo`} />
+        <div className={styles.info}>
+          <div className={styles.badges}>
+            <p className={styles.company}>{job.company}</p>
+            {job.new && <Badge variant='primary' text='new!' />}
+            {job.featured && <Badge variant='dark' text='featured' />}
+          </div>
+          <p className={styles.position}>{job.position}</p>
+          <ul className={styles.details}>
+            <li>{job.postedAt}</li>
+            <li>{job.contract}</li>
+            <li>{job.location}</li>
+          </ul>
         </div>
-        <p className={styles.position}>{job.position}</p>
-        <ul className={styles.details}>
-          <li>{job.postedAt}</li>
-          <li>{job.contract}</li>
-          <li>{job.location}</li>
-        </ul>
       </div>
       <hr />
       <div className={styles.categories}>
         {categories.map((category, index) => (
-          <button key={index} onClick={() => onClickAddFilter(category)}>{category}</button>
+          <button key={index} onClick={() => onClickAddFilter(category)}>
+            {category}
+          </button>
         ))}
       </div>
     </article>
